b45d8a0bcd12275431efcf06f1c20992cc6cdd6a
TVales <timothyc.vales@gmail.com>
2021-02-15 01:01:07 -0500

    comments added to explain tasks in arduino file

--- Project2/project2.ino
+++ Project2/project2.ino
@@ -3,12 +3,12 @@
 TaskHandle_t xHandle1 = NULL;
 TaskHandle_t xHandle2 = NULL;
 
-
-
 // the loop function runs over and over again forever
 void loop() {}
 
+// how to create two task bodies 
 
+// 1+i^4 
 static void testFunc1( void *pvParameters )
 {
 	(void) pvParameters;
@@ -19,6 +19,7 @@ static void testFunc1( void *pvParameters )
 	}	
 }
 
+// 1+((a^2)*i)
 static void testFunc2( void *pvParameters )
 { 
 	(void) pvParameters;	
@@ -34,13 +35,17 @@ int main( void )
 	char c1 = 'a';
 	char c2 = 'b';			
 
+  //initialize scheduler 
+  //initializes xExtended_TCB array 
 	vSchedulerInit();
 
+  //create periodic task 
+  //adds task to xExtended_TCB array
 	vSchedulerPeriodicTaskCreate(testFunc1, "t1", configMINIMAL_STACK_SIZE, &c1, 1, &xHandle1, pdMS_TO_TICKS(0), pdMS_TO_TICKS(800), pdMS_TO_TICKS(100), pdMS_TO_TICKS(800));
 	vSchedulerPeriodicTaskCreate(testFunc2, "t2", configMINIMAL_STACK_SIZE, &c2, 2, &xHandle2, pdMS_TO_TICKS(0), pdMS_TO_TICKS(400), pdMS_TO_TICKS(400), pdMS_TO_TICKS(400));
 
-
-
+  //start system 
+  //start scheduling tasks with specified scheduling policy
 	vSchedulerStart();
 
 	/* If all is well, the scheduler will now be running, and the following line
@@ -48,4 +53,3 @@ int main( void )
 	
 	for( ;; );
 }
-
